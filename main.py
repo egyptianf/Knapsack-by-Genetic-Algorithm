22b80cef8292f76d59bc80d946a95d594890084d
egyptianf <walied2@protonmail.com>
2019-10-27 20:20:35 +0200

    modified main to run on all test cases in input_example.txt file

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from knapsackGA import GA
 
 fileInput = FileInput("input_example.txt")
 fileInput.read()
-temp_ks = fileInput.knapsacks[0]
-ga = GA(temp_ks)
-fittest = ga.main()
-best_items = fittest.show_items(temp_ks.items)
-best_value = 0
-for item in best_items:
-    item.show()
-    best_value += item.value
-print(best_value)
+case = 1
+for knapsack in fileInput.knapsacks:
+    ga = GA(knapsack)
+    fittest = ga.main()
+    best_items = fittest.show_items(knapsack.items)
+    best_value = 0
+    for item in best_items:
+        # item.show()
+        best_value += item.value
+    print("Case: ", case, best_value)
+    case += 1
